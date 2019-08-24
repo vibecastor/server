327760a5154b0e78ba29c3b24cc817f729df9a6d
vibecastor <vibecastor@gmail.com>
2019-08-24 15:23:25 -0700

    inital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// route handler
+
+app.get('/', (req, res) => {
+  res.send({  hi: 'there' });
+});
+
+// listen on dynamic port variable from Heroku
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
