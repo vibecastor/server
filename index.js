7bde2bf264a639e32865b195c0e529d83e2b404f
vibecastor <vibecastor@gmail.com>
2019-08-27 23:43:06 -0700

    stripe config

--- index.js
+++ index.js
@@ -14,16 +14,13 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
 
 app.use(bodyParser.json());
-
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey],
   })
 );
-
 app.use(passport.initialize());
-
 app.use(passport.session());
 
 authRoutes(app);
